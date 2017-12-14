02c50989d625a7d09438c0c4fe19d2c1cebfa4f6
Menencia <menencia@gmail.com>
2017-12-14 14:52:22 +0100

    add 1/I/I episodes

--- src/app/data/episodes.1_1_1.ts
+++ src/app/data/episodes.1_1_1.ts
@@ -34,5 +34,79 @@ export const EPISODES = [
       duration: '2:50',
       version: 4
     }
+  },
+  {
+    title: 'L\'assaut des gobelins',
+    region: 'Caverne de Dalnakya',
+    video: {
+      yt: 'rahpbUUyfAA',
+      duration: '5:01',
+      version: 5
+    }
+  },
+  {
+    title: 'Voix ensorcelante',
+    region: 'Tour des sirènes',
+    video: {
+      yt: 'rahpbUUyfAA',
+      duration: '4:10',
+      version: 5
+    }
+  },
+  {
+    title: 'Souvenirs d\'un père',
+    region: 'Plaines de Dalnakya',
+    video: {
+      yt: '5ZL4rTzbnJM',
+      duration: '3:46',
+      version: 4
+    }
+  },
+  {
+    title: 'Ruée sur le château',
+    region: 'Catacombes de Grandshelt',
+    video: {
+      yt: '5nBLhrZlCi4',
+      duration: '2:08',
+      version: 4
+    }
+  },
+  {
+    title: 'Crise dans la capitale',
+    region: 'Château de Grandshelt',
+    video: {
+      yt: 'v9VUx8Si7Jw',
+      duration: '4:08',
+      version: 4
+    }
+  },
+  {
+    title: 'Les Six Jurés de Paladia',
+    region: 'Grandshelt, capitale royale',
+    isTown: true,
+    video: {
+      yt: '6W8v64NVYOU',
+      duration: '3:52',
+      version: 4
+    }
+  },
+  {
+    title: 'La présence de la jeune fille',
+    region: 'Route de Fulan',
+    video: {
+      yt: 'JaRALDYkZwg',
+      duration: '3:26',
+      version: 4
+    }
+  },
+  {
+    title: 'Départ',
+    region: 'Ville portuaire de Lodin',
+    isTown: true,
+    video: {
+      yt: 'jFUtcPT8mVU',
+      duration: '5:04',
+      version: 4
+    }
   }
 ];
