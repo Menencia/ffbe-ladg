da16c48bfe12e2f601acfc8e0957d6d3890bcda3
Menencia <menencia@gmail.com>
2017-12-14 19:19:51 +0100

    add se/5 episodes

--- src/app/data/episodes.se-5.ts
+++ src/app/data/episodes.se-5.ts
@@ -14,5 +14,69 @@ export const EPISODES = [
       duration: '5:32',
       version: 5
     }
+  },
+  {
+    title: 'Prémonition agitée',
+    video: {
+      yt: '5Y09UFnZnmk',
+      duration: '4:39',
+      version: 5
+    }
+  },
+  {
+    title: 'Les enfants perdus',
+    video: {
+      yt: 'djYHXCNNY64',
+      duration: '6:17',
+      version: 5
+    }
+  },
+  {
+    title: 'Sur les traces des enfants',
+    video: {
+      yt: 'UvN-UCZJsjc',
+      duration: '3:07',
+      version: 5
+    }
+  },
+  {
+    title: 'Un groupe d\'hérétiques',
+    video: {
+      yt: 'GmCmkBy6OWU',
+      duration: '5:15',
+      version: 5
+    }
+  },
+  {
+    title: 'Poursuite suicide',
+    video: {
+      yt: 'Psmovf3wXrc',
+      duration: '3:58',
+      version: 5
+    }
+  },
+  {
+    title: 'Gardien de l\'ordre',
+    video: {
+      yt: 'qMI-W_58rtw',
+      duration: '7:11',
+      version: 5
+    }
+  },
+  {
+    title: 'Une nouvelle ère',
+    video: {
+      yt: 'Tg3ufO4gKI0',
+      duration: '3:14',
+      version: 5
+    }
+  },
+  {
+    title: 'Le futur de Grandshelt',
+    video: {
+      yt: 'IQxkmbPWEr4',
+      duration: '4:19',
+      version: 5
+    }
   }
 ];
