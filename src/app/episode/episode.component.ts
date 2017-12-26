9817d78a4ec53a5a009ec5078ef1c1447cd7f59a
Menencia <menencia@gmail.com>
2017-12-26 14:54:33 +0400

    add previous/next buttons on episode view

--- src/app/episode/episode.component.ts
+++ src/app/episode/episode.component.ts
@@ -1,30 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from '../data.service';
 import { Season } from '../models/season';
 import { Chapter } from '../models/chapter';
 import { Episode } from '../models/episode';
+
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-episode',
   templateUrl: './episode.component.html',
   styleUrls: ['./episode.component.css']
 })
-export class EpisodeComponent implements OnInit {
+export class EpisodeComponent {
 
   public season: Season;
   public chapter: Chapter;
   public episode: Episode;
+  public episodePrevious: Episode;
+  public episodeNext: Episode;
   public player: YT.Player;
 
   constructor(
-    public activatedRoute: ActivatedRoute,
-    public data: DataService
-  ) { }
-
-  ngOnInit() {
-    this.activatedRoute.params
-    .subscribe((params: any) => {
+    public route: ActivatedRoute,
+    public data: DataService,
+    public router: Router
+  ) { 
+    route.params.subscribe((params: any) => {
       this.loadEpisode(params.episode);
     });
   }
@@ -34,6 +36,11 @@ export class EpisodeComponent implements OnInit {
     this.season = season;
     this.chapter = chapter;
     this.episode = episode;
+    this.episodePrevious = this.getPrevious();
+    this.episodeNext = this.getNext();
+    if (this.player) {
+      this.player.cueVideoById(episode.video.yt);
+    }
   }
 
   savePlayer(player) {
@@ -43,6 +50,50 @@ export class EpisodeComponent implements OnInit {
 
   onStateChange(event) {
     console.log('player state', event.data);
+  }
+
+  getPrevious() {
+    const episodeIndex = _.findIndex(this.chapter.episodes, this.episode);
+    let episodePrevious: Episode;
+    if (episodeIndex-1 >= 0) {
+      episodePrevious = this.chapter.episodes[episodeIndex-1];
+    } else {
+      const chapterIndex = _.findIndex(this.season.chapters, this.chapter);
+      let chapterPrevious: Chapter;
+      if (chapterIndex-1 >= 0) {
+        chapterPrevious = this.season.chapters[chapterIndex-1];
+        episodePrevious = _.last(chapterPrevious.episodes);
+      }
+    }
+    return episodePrevious;
+  }
+  
+  getNext() {
+    const episodeIndex = _.findIndex(this.chapter.episodes, this.episode);
+    let episodeNext: Episode;
+    if (episodeIndex+1 < this.chapter.episodes.length) {
+      episodeNext = this.chapter.episodes[episodeIndex+1];
+    } else {
+      const chapterIndex = _.findIndex(this.season.chapters, this.chapter);
+      let chapterNext: Chapter;
+      if (chapterIndex+1 < this.season.chapters.length) {
+        chapterNext = this.season.chapters[chapterIndex+1];
+        episodeNext = chapterNext.episodes[0];
+      }
+    }
+    return episodeNext;
+  }
+
+  previous() {
+    if (this.episodePrevious) {
+      this.router.navigate(['/episode/', this.episodePrevious.ref.replace(/\//g, '-')]);
+    }
+  }
+
+  next() {
+    if (this.episodeNext) {
+      this.router.navigate(['/episode/', this.episodeNext.ref.replace(/\//g, '-')]);
+    }
   }
 
 }
