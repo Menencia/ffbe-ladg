3aaf3a1d83ed8b993247fa4c0b7466c39066b91e
Menencia <menencia@gmail.com>
2018-01-15 00:26:46 +0100

    remove titles when empty

--- src/app/table-chapters/table-chapters.component.ts
+++ src/app/table-chapters/table-chapters.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Chapter } from '../models/chapter';
 
+import * as _ from 'lodash';
+
 @Component({
   selector: 'app-table-chapters',
   templateUrl: './table-chapters.component.html'
@@ -10,9 +12,14 @@ export class TableChaptersComponent implements OnInit {
   @Input()
   chapters: Chapter[];
 
+  haveTitles: boolean;
+
   constructor() { }
 
   ngOnInit() {
+    this.haveTitles = _.every(this.chapters, (chapter) => {
+      return chapter.title;
+    });
   }
 
 }
