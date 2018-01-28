2257823eb0e828152fab63486fada47a45dc21a2
Menencia <menencia@gmail.com>
2018-01-28 02:11:20 +0100

    redone 10 epz for 1-1-1

--- src/app/data/episodes.1_1_1.ts
+++ src/app/data/episodes.1_1_1.ts
@@ -2,27 +2,27 @@ export const EPISODES = [
   {
     title: 'Pluie d\'étoiles',
     video: {
-      yt: '0eGo3I8yOuU',
-      duration: '2:36',
-      version: 4
+      yt: '3kaV7vsFU3U',
+      duration: '2:35',
+      version: 6
     }
   },
   {
     title: 'Le destructeur vêtu de noir',
     region: 'Temple de la Terre',
     video: {
-      yt: 'ECJXlot3GfU',
-      duration: '6:40',
-      version: 4
+      yt: 'aen_naS2e9c',
+      duration: '5:55',
+      version: 6
     }
   },
   {
     title: 'L\'effet du Cristal',
     region: 'Bois de Latius',
     video: {
-      yt: '7Avi6dlV4HY',
-      duration: '3:47',
-      version: 4
+      yt: '979UYhTXtW8',
+      duration: '3:42',
+      version: 6
     }
   },
   {
@@ -30,9 +30,9 @@ export const EPISODES = [
     region: 'Ville de Mitra',
     isTown: true,
     video: {
-      yt: 'pPQ1lAImUqM',
-      duration: '2:50',
-      version: 4
+      yt: 'fBefiBU68b8',
+      duration: '2:42',
+      version: 6
     }
   },
   {
@@ -57,27 +57,27 @@ export const EPISODES = [
     title: 'Souvenirs d\'un père',
     region: 'Plaines de Dalnakya',
     video: {
-      yt: '5ZL4rTzbnJM',
-      duration: '3:46',
-      version: 4
+      yt: 'QPNybq8hG8M',
+      duration: '3:17',
+      version: 6
     }
   },
   {
     title: 'Ruée sur le château',
     region: 'Catacombes de Grandshelt',
     video: {
-      yt: '5nBLhrZlCi4',
-      duration: '2:08',
-      version: 4
+      yt: 'WhkNxGOLqwc',
+      duration: '1:35',
+      version: 6
     }
   },
   {
     title: 'Crise dans la capitale',
     region: 'Château de Grandshelt',
     video: {
-      yt: 'v9VUx8Si7Jw',
-      duration: '4:08',
-      version: 4
+      yt: 'NKCVaR4Ti2k',
+      duration: '3:42',
+      version: 6
     }
   },
   {
@@ -85,18 +85,18 @@ export const EPISODES = [
     region: 'Grandshelt, capitale royale',
     isTown: true,
     video: {
-      yt: '6W8v64NVYOU',
-      duration: '3:52',
-      version: 4
+      yt: 'ZH0otAl-T30',
+      duration: '3:51',
+      version: 6
     }
   },
   {
     title: 'La présence de la jeune fille',
     region: 'Route de Fulan',
     video: {
-      yt: 'JaRALDYkZwg',
-      duration: '3:26',
-      version: 4
+      yt: 'A8wueWV24KY',
+      duration: '3:03',
+      version: 6
     }
   },
   {
@@ -104,9 +104,9 @@ export const EPISODES = [
     region: 'Ville portuaire de Lodin',
     isTown: true,
     video: {
-      yt: 'jFUtcPT8mVU',
-      duration: '5:04',
-      version: 4
+      yt: 'zny1Sx52oAo',
+      duration: '5:03',
+      version: 6
     }
   }
 ];
