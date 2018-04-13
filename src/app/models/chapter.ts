8f77f8a37d0ce8294fb42d877089b90ea1811b33
Menencia <menencia@gmail.com>
2018-04-14 00:38:42 +0200

    fix url image

--- src/app/models/chapter.ts
+++ src/app/models/chapter.ts
@@ -188,7 +188,8 @@ export class Chapter {
   }
 
   getImage(): string {
-    return `https://firebasestorage.googleapis.com/v0/b/ffbe-ladg.appspot.com/o/images%2Fffbe_${this.getRefForUrl()}.jpg?alt=media`;
+    const ref = this.getRefForUrl().toLocaleLowerCase();
+    return `https://firebasestorage.googleapis.com/v0/b/ffbe-ladg.appspot.com/o/images%2Fffbe_${ref}.jpg?alt=media`;
   }
 
 }
