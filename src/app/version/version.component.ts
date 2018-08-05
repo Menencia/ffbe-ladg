a19898829f18139eaf76a765531925839bf4a919
Menencia <menencia@gmail.com>
2018-08-05 14:35:10 +0200

    fix versions (again)

--- src/app/version/version.component.ts
+++ src/app/version/version.component.ts
@@ -10,7 +10,8 @@ export class VersionComponent implements OnInit {
 
   constructor() {
     this.versions = [
-      {b: '#1', l: 'Cette version a été enregistrée depuis YT sans son.'},
+      {b: '#1', l: 'Cette version a été enregistrée depuis YT sans son et avec la présence de la souris (grr).'},
+      {b: '#2', l: 'Cette version a été enregistrée depuis YT sans son.'},
       {b: '#5', l: `Cette version est faite avec la version 5.x de Nox.
       Ce qui corrige un problème de son pendant les captures.`},
       {b: '#6', l: `Cette version est faite avec la version 6.x de Nox.`},
